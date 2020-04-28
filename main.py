7301d0d1803b93b10a27511ffc438a80910d8f6e
yuina-blend <moon.h14.dd2.as@gmail.com>
2020-04-28 14:20:30 +0900

    [add]エゴサ

---
+++ main.py
@@ -0,0 +1,37 @@
+import tweepy
+import requests
+import sys
+from datetime import timedelta
+import MyAPI
+
+class Listener(tweepy.StreamListener):
+    def on_status(self, status):
+        status.created_at += timedelta(hours=9)
+        print('------------------------------')
+        print(status.text)
+        print("{name}({screen}) {created} via {src}\n".format(name=status.author.name, screen=status.author.screen_name,created=status.created_at, src=status.source))
+        function.job(status.text, status.user.name, status.id,status.author.screen_name, status.retweeted)
+        return True
+
+    def on_error(self, status_code):
+        print('error: ' + str(status_code))
+        return True
+
+    def on_timeout(self):
+        print('Timeout...')
+        return True
+
+authR = tweepy.OAuthHandler(MyAPI.consumer_key_read, MyAPI.consumer_secret_read)
+authR.set_access_token(MyAPI.access_token_read, MyAPI.access_secret_read)
+
+listener = Listener()
+
+stream = tweepy.Stream(authR, listener)
+while True:
+    try:
+        stream.filter(track=["#お願いチュウニペンギン"])
+    except KeyboardInterrupt:
+        print("\nkeyboardInterrupt\n")
+        sys.exit()
+    except:
+        print('error! fuck!')
