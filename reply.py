8e00d0516f8c1c843516b04bb2b3bee21f147e2c
yuina-blend <moon.h14.dd2.as@gmail.com>
2020-06-24 00:01:15 +0900

    [add]条件（ミスアタとか）の生成を追加

--- reply.py
+++ reply.py
@@ -23,6 +23,17 @@ def serch_level(text, LV_start, LV_end):
             return str(i)
     return "ALL"
 
+def generating_challenge():
+    notes_decision = ("JUSTICE", "ATTACK", "MISS")
+    Riquest_JUSTICE = (1, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150)
+    Riquest_ATTACK_or_MISS = (1, 5, 10, 20, 30)
+    
+    choiced = random.choice(notes_decision)
+    if choiced == notes_decision[0]:
+        return [choiced, random.choice(Riquest_JUSTICE)]
+    else:
+        return [choiced, random.choice(Riquest_ATTACK_or_MISS)]
+
 # def serch_genre(text):
 #     genre_list = []
 #     ジャンルの指定をつくる
@@ -36,13 +47,13 @@ def reply(tw_text, tw_user_name, tw_id, tw_author_screen_name, tw_retweeted):
     selected_song = selected_song.rstrip()
     media = "data/song_media/" + selected_song + ".png"
     #条件を生成
-
+    challenge_request = generating_challenge()
     try:
         api.create_favorite(tw_id)
     except:
         pass
     tweet_url = "https://twitter.com/" + tw_author_screen_name + "/status/" + str(tw_id)
-    
+    tweet_buf = "僕の選んだ課題曲は" + "「" + selected_song + "」" + "!!\n" + challenge_request[0] + " " + str(challenge_request[1]) + "以下を目指そう!!\n"+ tweet_url
     try:
         api.update_with_media(filename=media, status=tweet_buf)
     except:
