040302975da37f82b249d7cbf5d59fb04fc54d46
yuina-blend <moon.h14.dd2.as@gnail.com>
2020-10-28 19:48:53 +0900

    コースモード生成（テスト）

--- course_mode.py
+++ course_mode.py
@@ -26,35 +26,36 @@ api = tweepy.API(auth)
 #     return "ALL"
 
 def find_level(tweet):
+    unable_to_choice = ['10.1', '10.2', '10.4', '10.9', '14.2', '14.3', '14.4', '14.5', '14.6', '14.7', '14.8', '14.9']
     levels = []
     tweet = tweet.replace('＋', '+')
-    for level in re.findall("1[0-3]\+|1[0-4]\.[0-9]|1[0-4]", tweet):
-        levels.append(level)
+    for level in re.findall("1[0-4]\+|1[0-4]\.[0-9]|1[0-4]", tweet):
+        if level not in unable_to_choice:
+            levels.append(level)
     if len(levels) == 0:
-        l = []
-        for i in range(100, 142):
-            l.append(str(i / 10))
-        not_there = ['10.1', '10.2', '10.4', '10.9']
-        for i in range(142, 150):
-            not_there.append(str(i / 10))
-        is_konton = False
-        random_song = ['']
-        # print(not_there)
-        while True:
-            is_konton = False
-            random_song[0] = random.choice(l)
-            # print(random_song[0])
-            for konton in not_there:
-                if random_song[0] == konton:
-                    is_konton = True
-                    break
-            if not is_konton:
-                return random_song
-    else:
-        return levels
+        return ['random']
+    return levels
 
 
 def choice_song(levels):
+    unable_to_choice = ['10.1', '10.2', '10.4', '10.9', '14.2', '14.3', '14.4', '14.5', '14.6', '14.7', '14.8', '14.9']
+    for i in range(len(levels)):
+        if levels[i] == '10':
+            level_a = ['10.0', '10.3', '10.5', '10.6']
+            levels[i] = random.choice(level_a)
+        elif levels[i] == '10+':
+            level_b = ['10.7', '10.8']
+            levels[i] = random.choice(level_b)
+        elif levels[i] == '14':
+            level_c = ['14.0', '14.1']
+            levels[i] = random.choice(level_c)
+        elif levels[i] == '14+':
+            levels[i] = random.choice([str(i / 10) for i in range(100, 142) if str(i / 10) not in unable_to_choice])
+    choice_is_random = False
+    if levels[0] == 'random':
+        levels = levels.remove('random')
+        levels = [str(i / 10) for i in range(100, 142) if str(i / 10) not in unable_to_choice]
+        choice_is_random = True
     choiced_songs = []
     for i in range(len(levels)):
         path = "data/"
@@ -75,9 +76,24 @@ def choice_song(levels):
         songs = []
         for song in pathlib.Path(path).glob("*.png"):
             songs.append({"file_path": song, "file_name": str(
-                song)[:-4].replace(path, '')})
+                song)[:-4].replace(path, ''), "level_path": path})
         choiced_songs.append(random.choice(songs))
-    return choiced_songs
+    if (not choice_is_random) and (len(choiced_songs) >= 3):
+        return choiced_songs
+    else:
+        all_songs = []
+        for path in choiced_songs:
+            for song in pathlib.Path(path["level_path"]).glob("*.png"):
+                all_songs.append({"file_path": song, "file_name": str(song)[:-4].replace(path["level_path"], ''), "level_path": path['level_path']})
+        if len(choiced_songs) <= 3:
+            while len(choiced_songs) < 3:
+                choiced_songs.append(random.choice(all_songs))
+            return choiced_songs
+        else:
+            song_returns = []
+            while len(song_returns) < 3:
+                song_returns.append(random.choice(all_songs))
+            return song_returns
 
 def generating_challenge():
     notes_decision = ("JUSTICE", "ATTACK", "MISS")
@@ -103,7 +119,8 @@ def reply(tw_text, tw_user_name, tw_id, tw_author_screen_name, tw_retweeted):
         print(song_info["file_path"])
         update_files.append(str(song_info["file_path"]))
         send_tweet += "「" + song_info["file_name"] + "」"
-    send_tweet += "JUSTICE1以下で終了 "
+    # send_tweet += "JUSTICE1以下で終了 "
+    # print(send_tweet)
     media_ids = []
     for filename in update_files:
         res = api.media_upload(filename)
@@ -119,3 +136,4 @@ def reply(tw_text, tw_user_name, tw_id, tw_author_screen_name, tw_retweeted):
     except:
         traceback.print_exc()
     
+# reply(tw_text="#お願いチュウニペンギン\n13+ 13+ 13", tw_author_screen_name="a", tw_id=1, tw_user_name="a", tw_retweeted=0)
